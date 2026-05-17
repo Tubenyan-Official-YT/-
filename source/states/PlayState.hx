51c2a216f5fed1bc6c0f32cc2a6d3084143e1d2c
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-05-17 15:53:50 +0900

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -77,16 +77,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['너 지금 딴겜하지?', 0.2], //From 0% to 19%
+		['뭐하냐?', 0.4], //From 20% to 39%
+		['겁내 못하네', 0.5], //From 40% to 49%
+		['못하네', 0.6], //From 50% to 59%
+		['별로야', 0.69], //From 60% to 68%
+		['나쁘지 않다?', 0.7], //69%
+		['괜찮네~', 0.8], //From 70% to 79%
+		['꽤 한다!', 0.9], //From 80% to 89%
+		['되게 잘하네~!', 1], //From 90% to 99%
+		['정말 최고야!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
