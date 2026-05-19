82705747812b507933e0bed7b12cc44ceaecb6c0
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-05-19 19:41:09 +0900

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -181,7 +181,7 @@ class TitleState extends MusicBeatState
 		}
 		titleText.animation.play('idle');
 		titleText.updateHitbox();
-		var customGraphic = FlxG.bitmap.add("assets/shared/images/TitleBack.png", "TitleBack_Custom");
+		var customGraphic = FlxG.bitmap.add("assets/shared/images/TitleBack.png",false, "TitleBack_Custom");
 		var black = new FlxSprite().loadGraphic(customGraphic); 
 		black.antialiasing = ClientPrefs.data.antialiasing; // 안티앨리어싱 설정 (선택사항)
 		black.setGraphicSize(FlxG.width, FlxG.height);// 가로 길이를 화면에 맞춤
