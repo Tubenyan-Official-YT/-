978e376c3bf1996fa98b298c78366aefb8035f4f
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-06-13 22:10:28 +0900

    Update AchievementsMenuState.hx

--- source/states/AchievementsMenuState.hx
+++ source/states/AchievementsMenuState.hx
@@ -75,10 +75,11 @@ class AchievementsMenuState extends MusicBeatState
 			var spr:FlxSprite = new FlxSprite(440, grpOptions.members.length * 180).loadGraphic(graphic);
 			spr.scrollFactor.x = 0;
 			spr.screenCenter(X);
+			spr.x = spr.x - 100
 			spr.ID = options.indexOf(option);
 			spr.antialiasing = hasAntialias;
-			var name:FlxText = new FlxText(spr.x + spr.width + 20, spr.y - 20);
-			var desc:FlxText = new FlxText(spr.x + spr.width + 20, spr.y - 5);
+			var name:FlxText = new FlxText(spr.x + spr.width + 20, spr.y);
+			var desc:FlxText = new FlxText(spr.x + spr.width + 20, spr.y + 20);
 			name.text = option.displayName;
 			desc.text = option.description;
 			name.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE);
@@ -192,7 +193,7 @@ class AchievementsMenuState extends MusicBeatState
     	}
     	else progressBar.percent = 0;
 
-    	camFollow.setPosition(0, grpOptions.members[curSelected].getGraphicMidpoint().y);
+    	camFollow.setPosition(0, grpOptions.members[curSelected][0].getGraphicMidpoint().y);
 
     	for(i in 0...grpOptions.members.length)
         	grpOptions.members[i].alpha = (i == curSelected) ? 1 : 0.6;
