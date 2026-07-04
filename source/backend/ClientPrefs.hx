17deacb1c28fd84f5bd2554c21dbfd2920c3a03d
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-07-04 21:00:46 +0900

    Update ClientPrefs.hx

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -174,25 +174,10 @@ class ClientPrefs {
 	public static function loadPrefs() {
 		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
 
-		// 모든 필드를 차별 없이 순회하며 데이터 정합성을 맞춥니다.
 		for (key in Reflect.fields(data))
-		{
-			if (key == 'gameplaySettings') continue;
-
-			// 세이브 파일에 이미 데이터가 존재한다면 그 값을 가져옵니다.
-			if (Reflect.hasField(FlxG.save.data, key)) {
+			if (key != 'gameplaySettings' && Reflect.hasField(FlxG.save.data, key))
 				Reflect.setField(data, key, Reflect.field(FlxG.save.data, key));
-			}
-			// 세이브 파일에 데이터가 없다면(새로 추가된 변수 등), 구조체의 기본값으로 파일에 동등하게 채워줍니다.
-			else {
-				var defaultValue = Reflect.field(defaultData, key);
-				Reflect.setField(FlxG.save.data, key, defaultValue);
-			}
-		}
 		
-		// 구조적 규칙에 의해 갱신된 세이브 데이터를 하드디스크에 기록합니다.
-		FlxG.save.flush();
-
 		if(Main.fpsVar != null)
 			Main.fpsVar.visible = data.showFPS;
 
@@ -251,7 +236,7 @@ class ClientPrefs {
 			reloadVolumeKeys();
 		}
 	}
-
+	
 	inline public static function getGameplaySetting(name:String, defaultValue:Dynamic = null, ?customDefaultValue:Bool = false):Dynamic
 	{
 		if(!customDefaultValue) defaultValue = defaultData.gameplaySettings.get(name);
