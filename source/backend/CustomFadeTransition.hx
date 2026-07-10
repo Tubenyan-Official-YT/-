d8faa6b4a22431e6eb921a5a91ed511d8ea1f4df
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-07-11 01:43:42 +0900

    Update CustomFadeTransition.hx

--- source/backend/CustomFadeTransition.hx
+++ source/backend/CustomFadeTransition.hx
@@ -6,7 +6,6 @@ import flixel.FlxCamera;
 import flixel.util.FlxColor;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
-import flixel.addons.transition.FlxTransitionableState;
 import backend.Paths;
 
 class CustomFadeTransition extends MusicBeatSubstate {
@@ -17,7 +16,7 @@ class CustomFadeTransition extends MusicBeatSubstate {
 	var rightDoor:FlxSprite;
 	
 	var duration:Float;
-	var transCamera:FlxCamera; // 트랜지션 전용 카메라 변수 추가
+	var transCamera:FlxCamera;
 
 	public function new(duration:Float, isTransIn:Bool)
 	{
@@ -28,9 +27,8 @@ class CustomFadeTransition extends MusicBeatSubstate {
 
 	override function create()
 	{
-		// 1. 기존 카메라를 가져오지 않고, 항상 최상단에 그려질 독립 카메라를 새로 생성하여 추가합니다.
 		transCamera = new FlxCamera();
-		transCamera.bgColor = FlxColor.TRANSPARENT; // 배경은 투명하게 설정
+		transCamera.bgColor = FlxColor.TRANSPARENT;
 		FlxG.cameras.add(transCamera, false);
 		cameras = [transCamera];
 
@@ -40,7 +38,7 @@ class CustomFadeTransition extends MusicBeatSubstate {
 
 		leftDoor = new FlxSprite();
 		leftDoor.loadGraphic(Paths.image('fade/leftDoor'));
-		leftDoor.setGraphicSize(width, fullHeight); // 정확한 픽셀 크기 조절
+		leftDoor.setGraphicSize(width, fullHeight);
 		leftDoor.updateHitbox();
 		leftDoor.scrollFactor.set();
 		leftDoor.y = (fullHeight / 2) - (leftDoor.height / 2);
@@ -48,23 +46,35 @@ class CustomFadeTransition extends MusicBeatSubstate {
 
 		rightDoor = new FlxSprite();
 		rightDoor.loadGraphic(Paths.image('fade/rightDoor'));
-		rightDoor.setGraphicSize(width, fullHeight); // 정확한 픽셀 크기 조절
+		rightDoor.setGraphicSize(width, fullHeight);
 		rightDoor.updateHitbox();
 		rightDoor.scrollFactor.set();
 		rightDoor.y = (fullHeight / 2) - (rightDoor.height / 2);
 		add(rightDoor);
 
 		if (isTransIn) {
+			// 문이 열리는 연출 (새 스테이트 진입)
 			leftDoor.x = fullWidth / 2 - leftDoor.width;
 			rightDoor.x = fullWidth / 2;
 			FlxTween.tween(leftDoor, {x: -leftDoor.width}, duration, {ease: FlxEase.quadInOut});
 			FlxTween.tween(rightDoor, {x: fullWidth}, duration, {ease: FlxEase.quadInOut, onComplete: function(_) close()});
 		}
 		else {
+			// 문이 닫히는 연출 (기존 스테이트 퇴장)
 			leftDoor.x = -leftDoor.width;
 			rightDoor.x = fullWidth;
 			FlxTween.tween(leftDoor, {x: fullWidth / 2 - leftDoor.width}, duration, {ease: FlxEase.quadInOut});
-			FlxTween.tween(rightDoor, {x: fullWidth / 2}, duration, {ease: FlxEase.quadInOut, onComplete: function(_) close()});
+			FlxTween.tween(rightDoor, {x: fullWidth / 2}, duration, {
+				ease: FlxEase.quadInOut, 
+				onComplete: function(_) {
+					// [수정 핵심] 의도적인 close() 생략
+					// 문을 화면에 완전히 닫아둔 채로 스테이트만 즉시 교체하여 깜빡임을 완벽히 차단합니다.
+					if(finishCallback != null) {
+						finishCallback();
+						finishCallback = null;
+					}
+				}
+			});
 		}
 		super.create();
 	}
@@ -80,12 +90,11 @@ class CustomFadeTransition extends MusicBeatSubstate {
 		}
 	}
 
-	// 2. 메모리 누수 및 새 스테이트에서 카메라가 무한히 쌓이는 현상을 막기 위해 destroy를 오버라이드합니다.
 	override function destroy()
 	{
-		super.destroy();
 		if (transCamera != null) {
-			FlxG.cameras.remove(transCamera); // 사용이 끝난 트랜지션 카메라 제거
+			FlxG.cameras.remove(transCamera);
 		}
+		super.destroy();
 	}
 }
