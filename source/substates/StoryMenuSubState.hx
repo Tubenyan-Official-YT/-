b38a6ebf92f70cb06add504902a312ec5ecb478c
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-07-19 21:36:05 +0900

    Update StoryMenuSubState.hx

--- source/substates/StoryMenuSubState.hx
+++ source/substates/StoryMenuSubState.hx
@@ -188,11 +188,11 @@ class StoryMenuSubState extends MusicBeatSubstate
 			}
 
 			if (controls.UI_UP_P) {
-				changeWeek(1);
+				changeWeek(-1);
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
 			if (controls.UI_DOWN_P) {
-				changeWeek(-1);
+				changeWeek(1);
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
 			
@@ -227,7 +227,7 @@ class StoryMenuSubState extends MusicBeatSubstate
 
 		for (num => item in grpWeekText.members)
 		{
-			var desiredY:Float = 200 + (50 * (num - curWeek));
+			var desiredY:Float = 200 + (120 * (num - curWeek));
 			
 			// [교정] 매 프레임 애니메이션 오프셋 변화를 반영하여 왼쪽 위 끝(x, y)을 정확히 강제 고정
 			item.x = targetX - (item.frameWidth / 2) + item.offset.x;
