eaf1fc915f031911f9e7bb5f333b82275b6d0d4d
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-08-14 18:34:10 +0900

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -220,9 +220,10 @@ class FreeplayState extends MusicBeatState
 		var energyTxt:FlxText = new FlxText(0, 0, 0,Std.string(EnergySystem.currentEnergy) ,48,true);
 		energyTxt.setFormat(Paths.font("pixel-latin.ttf"), 17, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		energyTxt.borderSize = 1.5;
+		freeplayUIGrp.add(energyTxt);
 		energyTxt.x = startButton.x + startButton.width - energyTxt.width;
 		energyTxt.y = startButton.y + 5 + energyTxt.height;
-		freeplayUIGrp.add(energyTxt);
+		
 		
 		add(freeplayUIGrp);
 		
