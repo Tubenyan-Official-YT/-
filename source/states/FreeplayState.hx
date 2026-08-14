42fd0bc9681ebd8498be7e084c8305d9f28b93eb
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-08-14 18:07:27 +0900

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -213,7 +213,12 @@ class FreeplayState extends MusicBeatState
     	startButton.antialiasing = ClientPrefs.data.antialiasing;
     	freeplayUIGrp.add(startButton);
 		
-		energyText = new FlxText(startButton.x,)
+		var energyTxt:FlxText = new FlxText(startButton.x, 0, 0,Std.string(EnergySystem.currentEnergy) ,48,true);
+		energyTxt.setFormat(Paths.font("pixel-latin.ttf"), 17, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		energyTxt.borderSize = 1.5;
+		energyTxt.x = startButton.x + startButton.width - energyTxt.width;
+		energyTxt.y = startButton.y + 5 + energyTxt.height;
+		freeplayUIGrp.add(energyTxt);
 		
 		add(freeplayUIGrp);
 		
