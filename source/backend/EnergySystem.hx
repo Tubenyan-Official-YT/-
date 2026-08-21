f398c13bbb947d3a29455139ba4af241a6bff2eb
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-08-21 23:02:13 +0900

    Update EnergySystem.hx

--- source/backend/EnergySystem.hx
+++ source/backend/EnergySystem.hx
@@ -108,8 +108,9 @@ class EnergySystem
         save();
     }
     public static function spendLS(howmuch:Int):Void {
-        // 리더십빼기 (Int 타입 null 검사 제거 및 인자값 반영)
+        // 리더십빼기, 통솔력 추가하기 (Int 타입 null 검사 제거 및 인자값 반영)
         leaderShip -= howmuch;
+        addLS(maxEnergy);
         save();
     }
 }
