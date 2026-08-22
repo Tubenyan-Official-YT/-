d7ed8565885c797f973d0c92f301a8209cbb2532
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-08-22 22:02:26 +0900

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -75,6 +75,7 @@ class TitleState extends MusicBeatState
 		{
 			ClientPrefs.loadPrefs();
 			Language.reloadPhrases();
+			EnergySystem.init();
 		}
 
 		curWacky = FlxG.random.getObject(getIntroTextShit());
@@ -110,8 +111,6 @@ class TitleState extends MusicBeatState
 		else
 			startIntro();
 		#end
-			
-		EnergySystem.init();
 		var testEnergy:FlxText = new FlxText(0,0,0,Std.string(EnergySystem.currentEnergy) ,48,true);
 		testEnergy.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		testEnergy.borderSize = 1.5; // 외곽선 두께 (1.2 ~ 2.0 권장)
