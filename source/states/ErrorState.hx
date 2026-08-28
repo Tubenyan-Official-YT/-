7649e1b0312f763c7960f2a1096f13aa6925e090
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-08-28 22:53:18 +0900

    Update ErrorState.hx

--- source/states/ErrorState.hx
+++ source/states/ErrorState.hx
@@ -19,7 +19,7 @@ class ErrorState extends MusicBeatState
 	public var errorText:FlxText;
 	override function create()
 	{
-		var bg = new FlxSprite().loadGraphic(Paths.image('Credits'));
+		var bg = new Window("errorWin", 100, true, 0.6);
 		bg.color = FlxColor.GRAY;
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 		add(bg);
