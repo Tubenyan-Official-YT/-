01d52321f4736ff1c285a8bddc736d654f376e2f
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-08-30 18:11:06 +0900

    Change package declaration to 'objects'

---
+++ source/objects/GlobalOverlay.hx
@@ -0,0 +1,39 @@
+package objects;
+
+import flixel.FlxBasic;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import states.PlayState;
+
+class GlobalOverlay extends FlxBasic
+{
+	public static var instance:GlobalOverlay;
+	public var sprite:FlxSprite;
+
+	public function new()
+	{
+		super();
+		instance = this;
+
+		// 표시할 오브젝트 생성 및 설정
+		sprite = new FlxSprite(10, 10).loadGraphic(Paths.image('logo')); // 원하는 이미지 경로 지정
+		sprite.scrollFactor.set(0, 0); // 화면 고정
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		if (sprite != null && sprite.active)
+			sprite.update(elapsed);
+	}
+
+	override public function draw()
+	{
+		super.draw();
+		// 현재 스테이트가 PlayState가 아닐 때만 화면에 출력
+		if (sprite != null && sprite.visible && !(FlxG.state is PlayState))
+		{
+			sprite.draw();
+		}
+	}
+}
