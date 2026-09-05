19c9325af3f0fbcfe4378ba93891d73507cf02ce
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-09-05 13:09:27 +0900

    Change GlobalOverlay addition to use addChild

--- source/Main.hx
+++ source/Main.hx
@@ -164,7 +164,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
 		addChild(new FlxGame(game.width, game.height, game.initialState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
-		FlxG.plugins.add(objects.GlobalOverlay.instance = new objects.GlobalOverlay());
+		addChild(objects.GlobalOverlay.instance = new objects.GlobalOverlay());
 
 		#if !mobile
 		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
