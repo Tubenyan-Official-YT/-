07df4f2c9658edc65b1c633664a81bde91b1527a
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-09-05 13:07:41 +0900

    Refactor GlobalOverlay to use Bitmap for overlays
    Refactored GlobalOverlay class to use Bitmap instead of FlxSprite for rendering. Removed unused update and draw methods.

--- source/objects/GlobalOverlay.hx
+++ source/objects/GlobalOverlay.hx
@@ -1,48 +1,30 @@
 package objects;
 
-import flixel.FlxBasic;
+import openfl.display.Bitmap;
+import openfl.display.Sprite;
 import flixel.FlxG;
-import flixel.FlxSprite;
 import states.PlayState;
 
-class GlobalOverlay extends FlxBasic
+class GlobalOverlay extends Sprite
 {
 	public static var instance:GlobalOverlay;
-	public var sprite:FlxSprite;
-	public var downSprite:FlxSprite;
+	public var topBitmap:Bitmap;
+	public var downBitmap:Bitmap;
 
 	public function new()
 	{
 		super();
 		instance = this;
 
-		// 표시할 오브젝트 생성 및 설정
-		sprite = new FlxSprite(0, 0).loadGraphic(Paths.image('overlay/topOverlay')); // 원하는 이미지 경로 지정
-		sprite.scrollFactor.set(0, 0); // 화면 고정
+		var topGraphic = Paths.image('overlay/topOverlay');
+		if (topGraphic != null)
+			addChild(topBitmap = new Bitmap(topGraphic.bitmap));
 
-		downSprite = new FlxSprite(0,0).loadGraphic(Paths.image('overlay/downOverlay'));
-		downSprite.y = FlxG.height - downSprite.height;
-		downSprite.scrollFactor.set(0, 0);
-	}
-
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-		if (sprite != null && sprite.active)
-			sprite.update(elapsed);
-		if (downSprite != null && downSprite.active)
-			downSprite.update(elapsed);
-	}
-
-	override public function draw()
-	{
-		super.draw();
-		// 현재 스테이트가 PlayState가 아닐 때만 화면에 출력
-		if (FlxG.state is PlayState) return;
-
-		if (sprite != null && sprite.visible)
-			sprite.draw();
-		if (downSprite != null && downSprite.visible)
-			downSprite.draw();
+		var downGraphic = Paths.image('overlay/downOverlay');
+		if (downGraphic != null)
+		{
+			addChild(downBitmap = new Bitmap(downGraphic.bitmap));
+			downBitmap.y = FlxG.height - downBitmap.height;
+		}
 	}
 }
