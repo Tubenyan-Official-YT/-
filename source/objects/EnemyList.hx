5b70412d1e1705b2210ff16814fa281ddf8a7418
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-09-06 18:00:43 +0900

    Create EnemyList.hx

---
+++ source/objects/EnemyList.hx
@@ -0,0 +1,26 @@
+package objects;
+
+class EnemyList extends Window {
+	var image:FlxSprite;
+
+	public function new(paths:String) {
+		super("enemyListWin", 30, true, false);
+
+		image = new FlxSprite(0, 0).loadGraphic(Paths.image(paths));
+		addItem("screenCenter", image);
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(this)) {
+			close();
+		}
+	}
+
+	public function close() {
+		destroyCam();
+		if (FlxG.state != null) FlxG.state.remove(this, true);
+		destroy();
+	}
+}
