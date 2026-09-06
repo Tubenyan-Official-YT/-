3b6dc9dbfbcbefbd1c8c46237d4c74c6f843f868
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-09-06 18:57:40 +0900

    Create EnemyListGroup class for enemy management

---
+++ source/objects/EnemyListGroup.hx
@@ -0,0 +1,13 @@
+package objects;
+
+class EnemyListGroup extends FlxSpriteGroup {
+	public function new(characterNames:Array<String>, spacing:Float = 90) {
+		super();
+
+		for (i in 0...characterNames.length) {
+			var enemy = new EnemyListCharacter(characterNames[i]);
+			enemy.x = i * spacing;
+			add(enemy);
+		}
+	}
+}
