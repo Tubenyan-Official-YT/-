0657f90504ff6d745198296fad05b50921a76c00
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-09-06 20:49:09 +0900

    Update EnemyList.hx

--- source/objects/EnemyList.hx
+++ source/objects/EnemyList.hx
@@ -3,24 +3,22 @@ package objects;
 class EnemyList extends Window {
 	var image:FlxSprite;
 	var list:EnemyListGroup;
+	public var closed:Bool = false;
 	public function new(name:Array<String>) {
 		super("enemyListWin", 30, true, false);
-		
 		list = new EnemyListGroup(name, 90);
 		addItem("screenCenter", list);
 	}
-
 	override function update(elapsed:Float) {
 		super.update(elapsed);
-
 		if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(this)) {
 			close();
 		}
 	}
-
 	public function close() {
-		destroyCam();
-		if (FlxG.state != null) FlxG.state.remove(this, true);
-		destroy();
+    	closed = true;
+    	destroyCam();
+    	if (FlxG.state != null) FlxG.state.remove(this, true);
+    	destroy();
 	}
 }
