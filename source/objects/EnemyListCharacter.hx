d11b784474817844a0a4f25279934a473e6d9a52
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-09-06 19:50:26 +0900

    Update EnemyListCharacter.hx

--- source/objects/EnemyListCharacter.hx
+++ source/objects/EnemyListCharacter.hx
@@ -1,14 +1,17 @@
 package objects;
 
+import backend.EasyJson;
+
 class EnemyListCharacter extends FlxSprite {
 	var charName:String;
 
-	public function new(characterName:String) {
+	public function new(characterName:String) 
 		super();
 		charName = characterName;
 
 		var charJson = new EasyJson(Paths.getPath('characters/$charName.json', TEXT));
-		var idleAnim:Dynamic = Lambda.find(charJson.get('animations'), a -> a['anim'] == 'idle');
+		var animations:Array<Dynamic> = charJson.get('animations');
+		var idleAnim:Dynamic = Lambda.find(animations, a -> a['anim'] == 'idle');
 
 		this.frames = Paths.getSparrowAtlas(charJson.get('image'));
 		this.animation.addByPrefix("idle", "idle", idleAnim['fps'], idleAnim['loop']);
